3bd9fb7ecbac5fff3d9170157daf15e947e04503
Andrey Rusanov <rusanov.andrey.mail@gmail.com>
2016-01-20 22:32:14 +0200

    Fix README issues

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='nose_env_config',
-    version='0.1.1',
+    version='0.1.2',
     packages=['nose_env_config'],
     url='https://github.com/andreyrusanov/nose_env_config',
     license='MIT',
